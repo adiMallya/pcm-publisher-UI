91efe8e1bd364f09232e7432da3bcec2fa661e4a
adiMallya <adimallya99@gmail.com>
2024-05-06 00:46:21 +0530

    ADD: Theme, Routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { ThemeProvider } from "@emotion/react";
+import { Route, Routes } from "react-router-dom";
+import { theme } from "src/styles";
+import { Home } from "./pages";
+
 const App: React.FC = () => {
   return (
     <>
-      <p>Hello World</p>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/searchAndReplace" element={<Home />} />
+        </Routes>
+      </ThemeProvider>
     </>
   );
 };
