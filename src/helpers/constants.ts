ef2d573d602c80520526a53e702f2d606f91a363
Adithya Mallya <adimally@publicisgroupe.net>
2024-05-07 00:31:57 +0530

    ADD: Base url, Bu list

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,10 +1,23 @@
+export const BASE_API = 'https://localhost:7184';
+
 export const pages: Array<{ name: string, link: string }> = [
     {
         name: 'Clone',
-        link: '/'
+        link: '/clone'
     },
     {
         name: 'Replace',
         link: '/searchAndReplace'
     }
+];
+
+export const BUList: Array<{ buId: string, buName: string }> = [
+    {
+        buId: '1',
+        buName: 'TEST BU'
+    },
+    {
+        buId: '53f8cdc5-f8c9-4f2b-a7dd-75b24dd12773',
+        buName: 'METT Orchestration TEST'
+    },
 ];
