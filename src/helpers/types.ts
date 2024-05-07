d2e4806d8c0196cb0c0dc23f49cd58d7ef4c9826
Adithya Mallya <adimally@publicisgroupe.net>
2024-05-07 20:53:44 +0530

    ADD: Required types for endpoints

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -11,3 +11,29 @@ export interface IPublishFormInput {
   targetFolder?: string;
   targetCampaignId?: string;
 }
+// Folders
+export interface IFolderResponse {
+  folderID: string;
+  folderName: string;
+}
+// Campaigns
+export interface ICampaignRequest {
+  folderID: string;
+  pageNo: Number;
+  recordsPerPage: number;
+}
+export interface Campaign {
+  campaignID: string;
+  campaignName: string;
+}
+export interface ICampaignResponse {
+  campaigns: Campaign[];
+  totalRecords: Number;
+}
+// Search Campaigns
+export interface ISearchCampaignRequest {
+  folderID: string;
+  pageNo: Number;
+  recordsPerPage: number;
+  searchName: string;
+}
