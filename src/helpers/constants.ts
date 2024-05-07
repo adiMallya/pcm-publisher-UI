e121beb06a931433591611e22c6d48bdd0e498eb
Adithya Mallya <adimally@publicisgroupe.net>
2024-05-07 20:54:53 +0530

    ADD: Column list for grid

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,23 +1,30 @@
-export const BASE_API = 'https://localhost:7184';
+import { GridColDef } from "@mui/x-data-grid";
 
-export const pages: Array<{ name: string, link: string }> = [
-    {
-        name: 'Clone',
-        link: '/clone'
-    },
-    {
-        name: 'Replace',
-        link: '/searchAndReplace'
-    }
+export const BASE_API = "https://localhost:7184";
+
+export const pages: Array<{ name: string; link: string }> = [
+  {
+    name: "Clone",
+    link: "/clone",
+  },
+  {
+    name: "Replace",
+    link: "/searchAndReplace",
+  },
 ];
 
-export const BUList: Array<{ buId: string, buName: string }> = [
-    {
-        buId: '1',
-        buName: 'TEST BU'
-    },
-    {
-        buId: '53f8cdc5-f8c9-4f2b-a7dd-75b24dd12773',
-        buName: 'METT Orchestration TEST'
-    },
+export const BUList: Array<{ buId: string; buName: string }> = [
+  {
+    buId: "1",
+    buName: "TEST BU",
+  },
+  {
+    buId: "53F8CDC5-F8C9-4F2B-A7DD-75B24DD12773",
+    buName: "METT Orchestration TEST",
+  },
+];
+
+export const DataColumnList: GridColDef[] = [
+  { field: "campaignID", headerName: "Message ID", width: 130 },
+  { field: "campaignName", headerName: "Message Name", width: 200 },
 ];
