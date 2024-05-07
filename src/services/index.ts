45698754ae09d1a445b1fa2a166f4871d4290572
Adithya Mallya <adimally@publicisgroupe.net>
2024-05-07 20:53:58 +0530

    ADD: Fetch data endpoints, search endpoints

---
+++ src/services/index.ts
@@ -0,0 +1,59 @@
+import axios from "axios";
+import {
+  BASE_API,
+  ICampaignRequest,
+  ISearchCampaignRequest,
+} from "src/helpers";
+
+export const fetchFoldersByBUId = async (buId: string) => {
+  try {
+    const response = await axios.get(`${BASE_API}/folders/${buId}`);
+    if (response.status !== 200)
+      throw new Error(
+        `Failed to fetch folders, server responded with status: ${response.status}`
+      );
+    return response.data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const fetchCampaignsForFolderIdByPage = async (
+  reqBody: ICampaignRequest
+) => {
+  try {
+    const response = await axios.post(
+      `${BASE_API}/campaigns/${reqBody.folderID}`,
+      {
+        ...reqBody,
+      }
+    );
+    if (response.status !== 200)
+      throw new Error(
+        `Failed to fetch campaigns for folder: ${reqBody.folderID}, server responded with status: ${response.status}`
+      );
+    return response.data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const searchCampaignsByName = async ({
+  searchName,
+  ...rest
+}: ISearchCampaignRequest) => {
+  try {
+    const response = await axios.post(`${BASE_API}/campaigns`, {
+      campaignName: searchName,
+      campaignType: 0,
+      ...rest,
+    });
+    if (response.status !== 200)
+      throw new Error(
+        `Failed to fetch campaigns, server responded with status: ${response.status}`
+      );
+    return response.data;
+  } catch (err) {
+    console.error(err);
+  }
+};
