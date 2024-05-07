564adb1a28289b3d204cd659affa56f756413008
Adithya Mallya <adimally@publicisgroupe.net>
2024-05-08 03:25:52 +0530

    REFACTOR: Data column flex; BUList updated

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -15,16 +15,12 @@ export const pages: Array<{ name: string; link: string }> = [
 
 export const BUList: Array<{ buId: string; buName: string }> = [
   {
-    buId: "1",
-    buName: "TEST BU",
-  },
-  {
-    buId: "53F8CDC5-F8C9-4F2B-A7DD-75B24DD12773",
-    buName: "METT Orchestration TEST",
+    buId: "9899F5AC-5B95-4622-AA45-20518BC9A213",
+    buName: "TEST BU FOR DEMO",
   },
 ];
 
 export const DataColumnList: GridColDef[] = [
-  { field: "campaignID", headerName: "Message ID", width: 130 },
-  { field: "campaignName", headerName: "Message Name", width: 200 },
+  { field: "campaignID", headerName: "Message ID", flex: 1 },
+  { field: "campaignName", headerName: "Message Name", flex: 1 },
 ];
