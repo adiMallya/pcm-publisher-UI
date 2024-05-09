45a6badec8d399f81331672ea09f19d413e1a797
Adithya Mallya <adimally@publicisgroupe.net>
2024-05-09 19:28:42 +0530

    CHORE: Updated global styles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,37 +1,39 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme, alpha } from "@mui/material/styles";
 
 export const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#f5f5f5',
-            light: '#fff',
-            dark: '#78909c'
-        },
-        secondary: {
-            main: '#4ba1a1'
-        },
-        text: {
-            primary: "#000",
-            secondary: "#fff"
-        }
-
+  palette: {
+    primary: {
+      main: "#f5f5f5",
+      light: "#fff",
+      dark: "#78909c",
     },
-    components: {
-        MuiButton: {
-            styleOverrides: {
-                root: {
-                    backgroundColor: '#4ba1a1',
-                    color: '#fff'
-                },
-            },
+    secondary: {
+      main: "#4ba1a1",
+    },
+    text: {
+      primary: "#000",
+      secondary: "#fff",
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#4ba1a1",
+          color: "#fff",
+          "&:hover": {
+            backgroundColor: alpha("#4ba1a1", 0.9),
+          },
         },
-        MuiAppBar: {
-            styleOverrides: {
-                root: {
-                    backgroundColor: '#2b2b2b',
-                    color: '#fff'
-                },
-            },
-        }
-    }
+      },
+    },
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#2b2b2b",
+          color: "#fff",
+        },
+      },
+    },
+  },
 });
