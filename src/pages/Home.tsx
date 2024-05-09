2a2e01169a7cfef2b69e23027921032f7cb50f60
Adithya Mallya <adimally@publicisgroupe.net>
2024-05-09 19:28:10 +0530

    ADD: Monitor jobs grid

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,55 @@
-import { Toolbar } from "@mui/material";
+import { Stack, Typography, CircularProgress } from "@mui/material";
+import { GridSlots } from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
 import { SiteLayout } from "src/components";
-
+import { StyledDataGrid } from "src/components";
+import { NoRows } from "src/components/NoRows";
+import { JobsGridColumnList } from "src/helpers";
+import { fetchJobs } from "src/services";
 export const Home: React.FC = () => {
+  const [records, setRecords] = useState({
+    isLoading: false,
+    data: [],
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setRecords((old) => ({ ...old, isLoading: true }));
+      const jobs = await fetchJobs();
+      if (jobs) {
+        setRecords((old) => ({
+          ...old,
+          data: jobs,
+        }));
+      }
+      setRecords((old) => ({ ...old, isLoading: false }));
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <SiteLayout>
-      <p>Home Page</p>
+      <Stack
+        style={{ height: 400, width: "100%" }}
+        direction={"column"}
+        padding={3}
+      >
+        <Typography variant="h4" component="h2" align="center" gutterBottom>
+          Monitor Jobs
+        </Typography>
+        <StyledDataGrid
+          paginationMode="server"
+          rowCount={records.data?.length}
+          rows={records?.data}
+          columns={JobsGridColumnList}
+          loading={records?.isLoading}
+          slots={{
+            loadingOverlay: CircularProgress as GridSlots["loadingOverlay"],
+            noRowsOverlay: NoRows,
+          }}
+        />
+      </Stack>
     </SiteLayout>
   );
 };
